c3feb9c76d0b0365b9f5b88a92fc7aa7ff3847ef
zhifeng ou <zhifengou@hotmail.com>
2019-04-04 20:33:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function printBadges(names) {
   for(let i = 0; i < names.length; i++) {
     console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
   }
-  return names;
 }
  function tailsNeverFails() {
   let tails = 0;
